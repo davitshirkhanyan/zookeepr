9cac55689e1be3b24ec61a2846e642265ab68db7
Davit Shirkhanyan <davit.shirkhanyan@gmail.com>
2021-02-25 21:48:40 -0800

    add route to html files

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const { animals } = require('./data/animals');
 
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
@@ -10,7 +11,8 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
-const { animals } = require('./data/animals');
+app.use(express.static('public'));
+
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -74,7 +76,7 @@ function createNewAnimal(body, animalsArray) {
         JSON.stringify({ animals: animalsArray }, null, 2)
     );
     // return finished code to post route for response
-    return animal;;
+    return animal;
 
 };
 
@@ -96,6 +98,20 @@ app.get('/api/animals/:id', (req, res) => {
     }
 });
 
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(
+        path.join(__dirname, './public/index.html')
+    );
+});
+
 app.post('/api/animals', (req, res) => {
     // set id based on what the next index of the array will be
     req.body.id = animals.length.toString();
@@ -111,6 +127,7 @@ app.post('/api/animals', (req, res) => {
 
     res.json(req.body);
 });
+
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${ PORT }`);
